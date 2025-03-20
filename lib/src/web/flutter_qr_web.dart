d73de874767c2e542f643d37612111548c4c1575
jmolins <chemamolins@gmail.com>
2025-03-20 17:27:26 +0100

    Add dart:ui_web for in order to use platformViewRegistry

--- lib/src/web/flutter_qr_web.dart
+++ lib/src/web/flutter_qr_web.dart
@@ -5,6 +5,7 @@ import 'dart:core';
 import 'dart:html' as html;
 import 'dart:js_util';
 import 'dart:ui' as ui;
+import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/material.dart';
 
@@ -33,12 +34,10 @@ class WebQrView extends StatefulWidget {
   @override
   _WebQrViewState createState() => _WebQrViewState();
 
-  static html.DivElement vidDiv =
-      html.DivElement(); // need a global for the registerViewFactory
+  static html.DivElement vidDiv = html.DivElement(); // need a global for the registerViewFactory
 
   static Future<bool> cameraAvailable() async {
-    final sources =
-        await html.window.navigator.mediaDevices!.enumerateDevices();
+    final sources = await html.window.navigator.mediaDevices!.enumerateDevices();
     // List<String> vidIds = [];
     var hasCam = false;
     for (final e in sources) {
@@ -66,8 +65,7 @@ class _WebQrViewState extends State<WebQrView> {
   html.VideoElement video = html.VideoElement();
   String viewID = 'QRVIEW-' + DateTime.now().millisecondsSinceEpoch.toString();
 
-  final StreamController<Barcode> _scanUpdateController =
-      StreamController<Barcode>();
+  final StreamController<Barcode> _scanUpdateController = StreamController<Barcode>();
   late CameraFacing facing;
 
   Timer? _frameIntervall;
@@ -81,8 +79,7 @@ class _WebQrViewState extends State<WebQrView> {
     // video = html.VideoElement();
     WebQrView.vidDiv.children = [video];
     // ignore: UNDEFINED_PREFIXED_NAME
-    ui.platformViewRegistry
-        .registerViewFactory(viewID, (int id) => WebQrView.vidDiv);
+    ui_web.platformViewRegistry.registerViewFactory(viewID, (int id) => WebQrView.vidDiv);
     // giving JavaScipt some time to process the DOM changes
     Timer(const Duration(milliseconds: 500), () {
       start();
@@ -92,8 +89,7 @@ class _WebQrViewState extends State<WebQrView> {
   Future start() async {
     await _makeCall();
     _frameIntervall?.cancel();
-    _frameIntervall =
-        Timer.periodic(const Duration(milliseconds: 200), (timer) {
+    _frameIntervall = Timer.periodic(const Duration(milliseconds: 200), (timer) {
       _captureFrame2();
     });
   }
@@ -137,8 +133,8 @@ class _WebQrViewState extends State<WebQrView> {
       widget.onPermissionSet?.call(_controller!, true);
       _localStream = stream;
       video.srcObject = _localStream;
-      video.setAttribute('playsinline',
-          'true'); // required to tell iOS safari we don't want fullscreen
+      video.setAttribute(
+          'playsinline', 'true'); // required to tell iOS safari we don't want fullscreen
       await video.play();
     } catch (e) {
       cancel();
@@ -177,16 +173,14 @@ class _WebQrViewState extends State<WebQrView> {
     if (_localStream == null) {
       return null;
     }
-    final canvas =
-        html.CanvasElement(width: video.videoWidth, height: video.videoHeight);
+    final canvas = html.CanvasElement(width: video.videoWidth, height: video.videoHeight);
     final ctx = canvas.context2D;
     // canvas.width = video.videoWidth;
     // canvas.height = video.videoHeight;
     ctx.drawImage(video, 0, 0);
     final imgData = ctx.getImageData(0, 0, canvas.width!, canvas.height!);
 
-    final size =
-        Size(canvas.width?.toDouble() ?? 0, canvas.height?.toDouble() ?? 0);
+    final size = Size(canvas.width?.toDouble() ?? 0, canvas.height?.toDouble() ?? 0);
     if (size != _size) {
       setState(() {
         _setCanvasSize(size);
@@ -197,8 +191,7 @@ class _WebQrViewState extends State<WebQrView> {
       final code = jsQR(imgData.data, canvas.width, canvas.height);
       // ignore: unnecessary_null_comparison
       if (code != null && code.data != null) {
-        _scanUpdateController
-            .add(Barcode(code.data, BarcodeFormat.qrcode, code.data.codeUnits));
+        _scanUpdateController.add(Barcode(code.data, BarcodeFormat.qrcode, code.data.codeUnits));
       }
     } on NoSuchMethodError {
       // Do nothing, this exception occurs continously in web release when no
@@ -263,9 +256,7 @@ class QRViewControllerWeb implements QRViewController {
   @override
   Future<CameraFacing> flipCamera() async {
     // TODO: improve error handling
-    _state.facing = _state.facing == CameraFacing.front
-        ? CameraFacing.back
-        : CameraFacing.front;
+    _state.facing = _state.facing == CameraFacing.front ? CameraFacing.back : CameraFacing.front;
     await _state.start();
     return _state.facing;
   }
@@ -325,8 +316,7 @@ class QRViewControllerWeb implements QRViewController {
   }
 }
 
-Widget createWebQrView(
-        {onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
+Widget createWebQrView({onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
     WebQrView(
       onPlatformViewCreated: onPlatformViewCreated,
       onPermissionSet: onPermissionSet,
